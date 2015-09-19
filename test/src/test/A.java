e2450409a71897994d3d0b8551a5bd70e35b578c
jaejoo <jaejoo@jaejoo-PC>
2015-09-20 04:22:41 +0900

    new

---
+++ test/src/test/A.java
@@ -0,0 +1,24 @@
+package test;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "";
+		a="123";
+		a+="345";
+		a+=1;
+		a+=1;
+		a+=1;
+		a+=1;
+		System.out.println(123);
+		System.out.println(123);
+		System.out.println(123);
+		System.out.println(123);
+		System.out.println(123);
+		System.out.println(123);
+		System.out.println(123);
+		
+	}
+
+}
